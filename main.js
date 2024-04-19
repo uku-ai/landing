b849c31168d1bd6f02ddc970d03f28f5d33bf99c
Otto Mättas <ottomattas@gmail.com>
2024-04-19 08:44:21 +0300

    Prettify code syntax

--- main.js
+++ main.js
@@ -3,7 +3,9 @@
 ///////////////////////////////////////////
 // JavaScript to handle opening the modal and setting the industry
 function openForm(industry) {
-    const modal = new bootstrap.Modal(document.getElementById('waitingListModal'));
-    document.getElementById('userIndustry').value = industry; // Pre-fill the industry field
-    modal.show();
-  }
+	const modal = new bootstrap.Modal(
+		document.getElementById("waitingListModal")
+	);
+	document.getElementById("userIndustry").value = industry; // Pre-fill the industry field
+	modal.show();
+}
